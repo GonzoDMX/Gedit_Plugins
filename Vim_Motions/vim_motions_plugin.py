4d842421a39d9812d4384ab3e7c0a98cbceaa2bb
Andrew O'Shei <andrewoshei@gmail.com>
2023-11-24 16:36:06 +0100

    fixed some keyval decs

--- Vim_Motions/vim_motions_plugin.py
+++ Vim_Motions/vim_motions_plugin.py
@@ -23,29 +23,31 @@ class VimMotionPlugin(GObject.Object, Gedit.ViewActivatable):
 
     def update_status_bar(self, vim_mode):
         self.update_highlight_style(vim_mode)
-        statusbar = self.view.get_toplevel().get_statusbar()
-        context_id = statusbar.get_context_id("VimMode")
+        if self.view is not None:
+            statusbar = self.view.get_toplevel().get_statusbar()
+            context_id = statusbar.get_context_id("VimMode")
 
-        if vim_mode:
-            self.status_message_id = statusbar.push(context_id, "Vim mode")
-        else:
-            if hasattr(self, 'status_message_id'):
-                statusbar.remove(context_id, self.status_message_id)
+            if vim_mode:
+                self.status_message_id = statusbar.push(context_id, "Vim mode")
+            else:
+                if hasattr(self, 'status_message_id'):
+                    statusbar.remove(context_id, self.status_message_id)
 
     def update_highlight_style(self, vim_mode):
-        style_context = self.view.get_style_context()
+        if self.view is not None:
+            style_context = self.view.get_style_context()
 
-        if vim_mode:
-            css = b'.view { background-color: #F0E68C; }'
-            self.css_provider.load_from_data(css)
-            style_context.add_provider(self.css_provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
-        else:
-            style_context.remove_provider(self.css_provider)
+            if vim_mode:
+                css = b'.view { background-color: #F0E68C; }'
+                self.css_provider.load_from_data(css)
+                style_context.add_provider(self.css_provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
+            else:
+                style_context.remove_provider(self.css_provider)
 
     def on_key_press(self, widget, event):
         keyval = Gdk.keyval_name(event.keyval)
         state = event.state
-
+        print(keyval)
         if keyval == 'Escape':
             if not self.vim_mode:
                 self.vim_mode = True
@@ -66,7 +68,7 @@ class VimMotionPlugin(GObject.Object, Gedit.ViewActivatable):
         if keyval == '0':
             self.move_cursor_to_line_start()
             return True
-        elif keyval == '^':
+        elif keyval == 'asciicircum':
             self.move_cursor_to_first_non_blank_character()
             return True
         elif keyval == 'G':
@@ -87,7 +89,7 @@ class VimMotionPlugin(GObject.Object, Gedit.ViewActivatable):
         elif keyval == 'g':
             # Implement logic to detect 'gg' sequence
             return True
-        elif keyval == '$':
+        elif keyval == 'dollar':
             self.move_cursor_to_line_end()
             return True
         elif keyval == 'I':
@@ -168,6 +170,7 @@ class VimMotionPlugin(GObject.Object, Gedit.ViewActivatable):
         buffer = self.view.get_buffer()
         iter = buffer.get_end_iter()
         buffer.place_cursor(iter)
+        self.view.scroll_to_iter(iter, 0.0, True, 0.0, 1.0)
 
     def scroll_page(self, direction, full_page):
         adjustment = self.view.get_vadjustment()
